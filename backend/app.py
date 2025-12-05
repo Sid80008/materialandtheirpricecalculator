31c869ab6af0c5cb69c8a9e24437a64269bc4aaa
SIDDHARTH UMAJWAL <160970705+Sid80008@users.noreply.github.com>
2025-12-06 00:24:10 +0530

    Clean up formatting in app.py
    Remove unnecessary newline in app.py

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from .calculator import estimate_materials
-
 app = Flask(__name__)
 # CORS allows your React frontend to make requests to this Flask backend
 CORS(app) 
